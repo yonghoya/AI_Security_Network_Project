5ef99d15e7e698794eb035f8b56b58343c050aea
yonghoya <dydgh0608@kookmin.ac.kr>
2023-09-06 01:13:12 +0900

    checkurl 앱 중복으로 인해 제거

--- config/settings.py
+++ config/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'checkurl.apps.CheckurlConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
