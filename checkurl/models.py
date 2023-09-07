adceb2e7e637f0f6fb137d4462f862cd6b166e12
schsdh <105278230+schsdh@users.noreply.github.com>
2023-09-07 12:31:26 +0900

    Delete checkurl/models.py

--- checkurl/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class URLManager(models.Model):
-    url_type = models.CharField(max_length=20)
-    type_explanation = models.TextField()
-    malicious = models.BooleanField()
