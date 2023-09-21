684c9d59ebdbdeacf097204f697ec323c71e5c4c
schsdh <105278230+schsdh@users.noreply.github.com>
2023-09-21 11:29:46 +0900

    Delete urls.py

--- urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from checkurl import views
-
-urlpatterns = [
-    path('', views.checkurl_main, name='checkurl_main'),
-    path('api/endpoint/', views.url_check_endpoint, name='url_check_endpoint'),
-]
